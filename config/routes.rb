9c73348e0caa2bf250f22311d1c50b471f7d3e0f
Nguyen Anh Thi <nguyenanhthi@gogojungle.vn>
2017-02-18 00:59:11 +0700

    use nested resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :orders
-  resources :food_items
-	get 'menu' => 'home#menu'
+  resources :food_items do
+    resources :orders
+  end
+  get 'menu' => 'home#menu'
   get 'contact_us' => 'home#contact_us'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "home#index"
