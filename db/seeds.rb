09e3b0a79f14aa3a4d4f7be52b688f054a2cf144
Nguyen Anh Thi <nguyenanhthi@gogojungle.vn>
2017-02-18 19:43:26 +0700

    use Faker in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,23 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 if Section.count == 0
-	puts "Seeding section data..."
-	%w(Breakfast Lunch Dinner Drinks).each do |name|
-		Section.create!(name: name)
-	end
-	
+  puts "Seeding section data..."
+  %w(Breakfast Lunch Dinner Drinks).each do |name|
+    Section.create!(name: name)
+  end
+
 else
   puts "You already have some section data"
 end
+
+
+10.times do |i|
+  FoodItem.create!(
+      :name => Faker::Food.ingredient,
+      :description => Faker::Lorem.sentence,
+      :price => Faker::Number.between(5, 20),
+      :section_id => Faker::Number.between(1, 4),
+      :image_url => ''
+  )
+end
+puts "10 food items added. "
