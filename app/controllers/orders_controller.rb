ffee493a79a4afbcc5d27dac3e8d365e10669e6d
Nguyen Anh Thi <nguyenanhthi@gogojungle.vn>
2017-02-19 15:27:08 +0700

    add order info page & coupon code

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,18 +5,34 @@ class OrdersController < ApplicationController
     @order = @item.orders.build
   end
 
+  def show
+    @food_item = FoodItem.find(params[:food_item_id])
+    @order = @food_item.orders.find(params[:id])
+    @date_time = DateTime.parse(@order.created_at.to_s)
+    @total = @food_item.price * @order.quantity
+    if @order.coupon.eql? "CODERSCHOOL"
+      @total = @total / 2
+    end
+    @total = @total + (@order.delivery ? 1 : 0)
+  end
+
   def create
     @item = FoodItem.find(params[:food_item_id])
     @order = @item.orders.build order_params
 
     if @order.save
-      redirect_to menu_path, flash: {sucssess: "Thank you for your order!"}
+      redirect_to food_item_order_path(params[:food_item_id], @order.id), flash: {success: "Thank you for your order!"}
     else
       render 'new'
     end
   end
 
+  def index
+    @food_item = FoodItem.find(params[:food_item_id])
+    @orders = @food_item.orders.all
+  end
+
   def order_params
-    params.require(:order).permit(:quantity)
+    params.require(:order).permit(:quantity, :customer_name, :phone_number, :address, :delivery, :coupon)
   end
 end
